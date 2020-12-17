1dbada80ef5855c248ca09cf5577fc7ef62558d4
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-17 16:11:41 +0530

    XAMARIN-38338 - Sample and screenshot added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Helper/AccordionTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    #region AccordionTemplateSelector
+    public class AccordionTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate CustomTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return (item as ItemInfo).ID % 2 == 0 ? CustomTemplate : DefaultTemplate;
+        }
+    }
+    #endregion
+}
---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/ViewModel/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    public class ItemInfoRepository
+    {
+        #region Properties
+        public ObservableCollection<ItemInfo> Info { get; set; }
+        #endregion
+
+        #region Constructor
+        public ItemInfoRepository()
+        {
+            Info = new ObservableCollection<ItemInfo>();
+            Info.Add(new ItemInfo() { ID = 1, Name = "Cheese burger", Description = "Hamburger accompanied with melted cheese. The term itself is a portmanteau of the words cheese and hamburger. The cheese is usually sliced, then added a short time before the hamburger finishes cooking to allow it to melt." });
+            Info.Add(new ItemInfo() { ID = 2, Name = "Veggie burger", Description = "Veggie burger, garden burger, or tofu burger uses a meat analogue, a meat substitute such as tofu, textured vegetable protein, seitan (wheat gluten), Quorn, beans, grains or an assortment of vegetables, which are ground up and formed into patties." });
+            Info.Add(new ItemInfo() { ID = 3, Name = "Barbecue burger", Description = "Consists of a hamburger, with the patty topped with chili con carne." });
+            Info.Add(new ItemInfo() { ID = 4, Name = "Chili burger", Description = "Prepared with ground beef, mixed with onions and barbecue sauce, and then grilled. Once the meat has been turned once, barbecue sauce is spread on top and grilled until the sauce caramelizes." });
+        }
+        #endregion
+    }
+
+    public class ItemInfo
+    {
+        #region Properties
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Description { get; set; }
+        #endregion
+    }
+}
+
